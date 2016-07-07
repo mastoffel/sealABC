2381e1e2a03921748dce7ceba505336298fa70f4
Martin Stoffel <martin.adam.stoffel@gmail.com>
2016-07-07 10:14:29 +0200

    bug correct

--- R/read_excel_sheets.R
+++ R/read_excel_sheets.R
@@ -12,10 +12,10 @@
 read_excel_sheets <- function(data_path) {
 
     # sheet numbers to load
-    dataset_names <- excel_sheets(data_path)
+    dataset_names <- readxl::excel_sheets(data_path)
 
     load_dataset <- function(dataset_names) {
-        read_excel(data_path, sheet = dataset_names)
+        readxl::read_excel(data_path, sheet = dataset_names)
     }
     # load all datasets
     all_seals <- lapply(dataset_names, load_dataset)
