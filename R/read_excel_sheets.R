98000ef5bb00c67a21d2d7f99623d3c80ad068dd
Martin Stoffel <martin.adam.stoffel@gmail.com>
2016-07-07 09:48:42 +0200

    first commit

---
+++ R/read_excel_sheets.R
@@ -0,0 +1,24 @@
+#' read multiple excel sheets
+#'
+#' loads multiple data frames into list from xlsx and assigns the
+#' tab names from the spreadsheet to the list elements
+#'
+#'
+#' @param data_path path to excel file with multiple data.frames
+#'
+#'
+
+
+read_excel_sheets <- function(data_path) {
+
+    # sheet numbers to load
+    dataset_names <- excel_sheets(data_path)
+
+    load_dataset <- function(dataset_names) {
+        read_excel(data_path, sheet = dataset_names)
+    }
+    # load all datasets
+    all_seals <- lapply(dataset_names, load_dataset)
+    names(all_seals) <- dataset_names
+    all_seals
+}
